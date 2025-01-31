debd58471c47b419a57f5a92ad6a6a1e544e4f39
Sudesh Gama <sudeshgama@Mac.attlocal.net>
2025-01-31 11:04:48 -0600

    Added cors to work in 4200

--- src/handlers/employee.ts
+++ src/handlers/employee.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Response } from "express";
 import prisma from "../db";
 import { AuthenticationRequest } from "../models/auth.model";
-import { createJwt } from "../modules/auth";
+import { comparePasswords, createJwt, hashPassword } from "../modules/auth";
 
 export const createEmployee = async (req: AuthenticationRequest, res: Response, next: NextFunction) => {
   try {
@@ -10,17 +10,56 @@ export const createEmployee = async (req: AuthenticationRequest, res: Response, 
       res.send({ message: 'Bad Request' });
       return;
     }
+
+    if (req.body.name === undefined ||
+      req.body.email === undefined ||
+      req.body.password === undefined ||
+      req.body.role === undefined
+    ) {
+      res.status(400);
+      res.send({ message: 'Bad Request One or more property missing in the payload' });
+      return;
+    }
+
     const employee = await prisma.employee.create({
       data: {
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: await hashPassword(req.body.password),
         role: req.body.role
       }
     })
     const token = createJwt(employee);
-    res.json({ token });
+    res.json({ message: 'You have signed up successfully' });
   } catch (e) {
     next(e);
+  }
+}
+
+export const signIn = async (req: AuthenticationRequest, res: Response, next: NextFunction) => {
+  try {
+
+    if (Object.entries(req.body).length === 0 || Object.entries(req.body).length > 2) {
+      res.status(400);
+      res.send({ message: 'Bad Request' });
+      return;
+    }
+
+    const employee = await prisma.employee.findUnique({
+      where: { email: req.body.email }
+    });
+
+    const isValid = comparePasswords(req.body.password, employee.password);
+
+    if (!isValid) {
+      res.status(401);
+      res.send("Invalid username or Password");
+      return;
+    }
+
+    const token = createJwt(employee);
+    res.json({ token: token, email: employee.email, role: employee.role });
+  } catch (e) {
+    next(e)
   }
 }
